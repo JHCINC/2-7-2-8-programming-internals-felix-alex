ca7420707f1cfe3345ab60590e440ac85639e298
19200jhc <127165031+19200jhc@users.noreply.github.com>
2024-03-25 20:55:17 +0000

    made gui window

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from PIL import ImageTk, Image
 
 window = tk.Tk()
 window.title("Right Angle Calculator")
-window.geometry("690x605")
+window.geometry("1000x800")
 window.config(bg='#2b2b2b')
 window.resizable(width=False, height=False)
 
@@ -15,19 +15,10 @@ test = ImageTk.PhotoImage(traingle)
 trilabel = tk.Label(image=test)
 trilabel.image = test
 
-lang1 = tk.Label(text="Angle (°)")
-eang1 = tk.Entry(window,width=5)
-lang2 = tk.Label(text="Angle (°)")
-eang2 = tk.Entry(window,width=5)
-
 button = tk.Button(text="Calculate Sides/Angles", command='get_sides' and 'get_angles')
 
-lang1.place(x=175,y=218)
-eang1.place(x=175,y=248)
-lang2.place(x=370,y=340)
-eang2.place(x=450,y=375)
 heading.place(x=20,y=20)
 trilabel.place(x=20,y=70)
-button.place(x=260,y= 550)
+button.place(x=250, y= 600)
 
 tk.mainloop()
